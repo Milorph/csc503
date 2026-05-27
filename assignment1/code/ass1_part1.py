fd3549fd4323a2bb9bce9269f181d19e145c171d
robert <robertwinstonwidjaja1@gmail.com>
2026-05-27 14:20:49 -0700

    tuning hyperparameters, experimenting with different vales and params for each classifier

--- assignment1/code/ass1_part1.py
+++ assignment1/code/ass1_part1.py
@@ -3,6 +3,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import tree
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
 GLOBAL_SEED = 0
 
 df = pd.read_csv("../data/spambase_augmented.csv", header=None)
@@ -21,7 +22,7 @@ print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 # decision tree with pruning
 # https://scikit-learn.org/stable/modules/tree.html#minimal-cost-complexity-pruning
 alphas = [0.0, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1]
-depths = []
+depths = [2, 4, 6, 8, 10, 12, 14]
 
 
 print("--------------- Decision Tree with different ccp_alpha -----------------")
@@ -39,34 +40,87 @@ for alpha in alphas:
     train_error = (clf_decision_tree.predict(X_train) != y_train).mean() * 100
     
     
-    print(f"alpha={alpha:.6f} ---- accuracy={acc:.4f}%") 
-    print(f"train-error={train_error:.4f}%")
+    print(f"ccp_alpha={alpha:.6f} ---- accuracy={acc:.8f}%") 
+    print(f"train-error={train_error:.8f}%")
+
+print("--------------- Decision Tree with different max_depths -----------------")
+for depth in depths:
+    clf_decision_tree = tree.DecisionTreeClassifier(
+        random_state=GLOBAL_SEED,
+        ccp_alpha=1e-3,
+        max_depth=depth
+    ) # should try with different depths to prevent overfitting maybe
+
+    clf_decision_tree.fit(X_train, y_train)
+
+    y_pred = clf_decision_tree.predict(X_test) #predict with trained
+
+    acc = accuracy_score(y_test, y_pred) * 100
+    train_error = (clf_decision_tree.predict(X_train) != y_train).mean() * 100
+    
+    
+    print(f"max_depths={depth} ---- accuracy={acc:.8f}%") 
+    print(f"train-error={train_error:.8f}%")
 
 #Random forest ( no pruning )
 # https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html#
 
-clf_forest = RandomForestClassifier(n_estimators=100, max_features="sqrt",random_state=GLOBAL_SEED) #will experiment with different n estimators, start at sqrt
-clf_forest.fit(X_train, y_train)
-y_pred = clf_forest.predict(X_test) #predict with trained
+estimators = [50,100,150,200,250,300,350,400]
+max_features = ["sqrt", "log2", 50, 80, 110, 140, 170, 200]
 
-acc = accuracy_score(y_test, y_pred) * 100
-train_error = (clf_forest.predict(X_train) != y_train).mean() * 100
+print("--------------- Random Forest (features variation) -----------------")
+for feature in max_features:
+    clf_forest = RandomForestClassifier(max_features=feature,random_state=GLOBAL_SEED,  n_jobs=-1) 
+    clf_forest.fit(X_train, y_train)
+    y_pred = clf_forest.predict(X_test) #predict with trained
 
-print("--------------- Random Forest -----------------")
-print(f"n_estimators={100} ---- accuracy={acc:.4f}%") 
-print(f"train-error={train_error:.4f}%")
+    acc = accuracy_score(y_test, y_pred) * 100
+    train_error = (clf_forest.predict(X_train) != y_train).mean() * 100
+
+
+    print(f"n_estimators={100}, max_features={feature} ---- accuracy={acc:.8f}%") 
+    print(f"train-error={train_error:.8f}%")
+
+print("--------------- Random Forest (n_estimators variation) -----------------")
+for estimator in estimators:
+    clf_forest = RandomForestClassifier(n_estimators=estimator,random_state=GLOBAL_SEED,  n_jobs=-1) 
+    clf_forest.fit(X_train, y_train)
+    y_pred = clf_forest.predict(X_test) #predict with trained
+
+    acc = accuracy_score(y_test, y_pred) * 100
+    train_error = (clf_forest.predict(X_train) != y_train).mean() * 100
+
+
+    print(f"n_estimators={estimator} ---- accuracy={acc:.8f}%") 
+    print(f"train-error={train_error:.8f}%")
 #Boosted decision trees https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.AdaBoostClassifier.html
 
-clf_ada = AdaBoostClassifier(n_estimators=100,learning_rate=1,random_state=GLOBAL_SEED) #will experiment with different n estimators and learning_rate
-clf_ada.fit(X_train, y_train)
-y_pred = clf_ada.predict(X_test) #predict with trained
+depths = [1, 2, 3, 4, 5, 6, 7]
+print("--------------- adaboost (depth variation) -----------------")
+for depth in depths:
+    clf_ada = AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=depth), n_estimators=100,random_state=GLOBAL_SEED) 
+    clf_ada.fit(X_train, y_train)
+    y_pred = clf_ada.predict(X_test) #predict with trained
 
-acc = accuracy_score(y_test, y_pred) * 100
-train_error = (clf_ada.predict(X_train) != y_train).mean() * 100
+    acc = accuracy_score(y_test, y_pred) * 100
+    train_error = (clf_ada.predict(X_train) != y_train).mean() * 100
 
-print("--------------- Adaboost -----------------")
-print(f"n_estimators={100}, learning_rate={1.0} ---- accuracy={acc:.4f}%") 
-print(f"train-error={train_error:.4f}%")
+
+    print(f"n_estimators={100}, max_depth={depth} ---- accuracy={acc:.8f}%") 
+    print(f"train-error={train_error:.8f}%")
+
+print("--------------- adaboost (n_estimators variation) -----------------")
+for estimator in estimators:
+    clf_ada = AdaBoostClassifier(estimator=DecisionTreeClassifier(max_depth=1),n_estimators=estimator,random_state=GLOBAL_SEED) 
+    clf_ada.fit(X_train, y_train)
+    y_pred = clf_ada.predict(X_test) #predict with trained
+
+    acc = accuracy_score(y_test, y_pred) * 100
+    train_error = (clf_ada.predict(X_train) != y_train).mean() * 100
+
+
+    print(f"n_estimators={estimator} ---- accuracy={acc:.8f}%") 
+    print(f"train-error={train_error:.8f}%")
 
 #defining k-fold cross-validation
 
