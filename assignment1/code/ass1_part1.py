136ec267662ca7e1dcb2d3415d7dee949c88332d
robert <robertwinstonwidjaja1@gmail.com>
2026-05-31 15:38:38 -0700

    k-fold cross

--- assignment1/code/ass1_part1.py
+++ assignment1/code/ass1_part1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split 
 from sklearn import tree
 from sklearn.metrics import accuracy_score
@@ -176,7 +177,32 @@ for f in fractions:
 
 #defining k-fold cross-validation
 
-def k_fold_cross_valid():
-  return
+# pass the model, num folds, data, global seed
+def k_fold_cross_valid(model_select, k, X, y, random_seed=GLOBAL_SEED):
+    n = len(X)
 
+    #required shuffling the indices once to be considered a k-fold-cross-valid
+    rng = np.random.RandomState(random_seed)
+    idx = rng.permutation(n)
+
+    #size of each fold
+    fold_size = n // k
+    #slice each part of arr
+    folds = []
+    
+    for i in range(k):
+        start = i * fold_size #start and endpoints of each slice
+        end = (i+1) * fold_size if i < k - 1 else n
+        folds.append(idx[start:end])
+    
+    val_errors = []
+    for i in range(len(folds)):
+        held_out_idx = folds[i]
+        train_idx = np.concatenate([folds[j] for j in range(k) if j != i]) # put together all the other folds
+        model_select.fit(X.iloc[train_idx], y.iloc[train_idx])
+        err = (model_select.predict(X.iloc[held_out_idx]) != y.iloc[held_out_idx]).mean()
+        val_errors.append(err)
+    
+    print(np.mean(val_errors), val_errors)
+    return np.mean(val_errors), val_errors
 #plotting
